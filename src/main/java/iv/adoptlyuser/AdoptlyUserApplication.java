770e1cb44540110b02cbc307044782d56a4b50e5
ignacioVillarreal2003 <ignaciovillarreal20031231@gmail.com>
2025-10-10 16:53:41 -0300

    first commit

---
+++ src/main/java/iv/adoptlyuser/AdoptlyUserApplication.java
@@ -0,0 +1,13 @@
+package iv.adoptlyuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptlyUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdoptlyUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iv/adoptlyuser/AdoptlyUserApplicationTests.java
@@ -0,0 +1,13 @@
+package iv.adoptlyuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptlyUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
